a1f0b67b16f644d90002f2767dc913531f828e55
Gracie Lundell <gracielundell@gmail.com>
2017-05-22 15:01:45 -0700

    style case studies

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,3 @@
-$(document).ready(function() {
-    $(window).scroll(function() {
-        var distanceFromTop = $(document).scrollTop();
-        if (distanceFromTop >= $('.parallax-container').height())
-        {
-            $('.main-nav').addClass('fixed');
-        }
-        else
-        {
-            $('.main-nav').removeClass('fixed');
-        }
-    });
-    $('.scrollspy').scrollSpy({
-      scrollOffset: 0
-    });
+$(document).ready(function(){
+  $('.parallax').parallax();
 });
