e28377b7df487cf6544ae79bc750fad2530688e3
Gracie Lundell <gracielundell@gmail.com>
2017-05-22 09:58:33 -0700

    set up materialize, get header img and main-nav in place

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,16 @@
 $(document).ready(function() {
-  // shows active link
-  $('a').each(function() {
-    if($(this).prop('href') == window.location.href) {
-      $(this).addClass('active');
-    }
-  });
-
-// open blog posts
-  $("#angularVSember").click(function(){
-    if($(this).hasClass("open")) {
-      $(this).removeClass("open");
-      $("#blogPost2").slideUp("slow");
-    } else {
-      $(this).addClass("open");
-      $("#blogPost2").slideDown("slow");
-    }
-  });
-  $("#htmlBasics").click(function(){
-    if($(this).hasClass("open")) {
-      $(this).removeClass("open");
-      $("#blogPost1").slideUp("slow");
-    } else {
-      $(this).addClass("open");
-      $("#blogPost1").slideDown("slow");
-    }
-  });
-
+    $(window).scroll(function() {
+        var distanceFromTop = $(document).scrollTop();
+        if (distanceFromTop >= $('.parallax-container').height())
+        {
+            $('.main-nav').addClass('fixed');
+        }
+        else
+        {
+            $('.main-nav').removeClass('fixed');
+        }
+    });
+    $('.scrollspy').scrollSpy({
+      scrollOffset: 0
+    });
 });
