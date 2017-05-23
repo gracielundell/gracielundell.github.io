62a78ec6b794b73caecd76c10572c28d8d52f791
Gracie Lundell <gracielundell@gmail.com>
2017-05-23 00:07:00 -0700

    begin working on bio

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
   $('.parallax').parallax();
+  // $(window).scroll(function() {
+  //       var distanceFromTop = $(document).scrollTop();
+  //       if (distanceFromTop >= $('.parallax-container').height())
+  //       {
+  //           $('.main__nav').addClass('fixed');
+  //       }
+  //       else
+  //       {
+  //           $('.main__nav').removeClass('fixed');
+  //       }
+  //   });
 });
