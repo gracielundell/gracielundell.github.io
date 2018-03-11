c8d75754be8917b28fd5f2ded5937aa32c9d6782
Gracie Lundell <gracielundell@gmail.com>
2018-03-10 17:11:59 -0800

    add code for moving between images and opacity on current image

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,36 @@
-$(document).ready(function(){
-  $('.parallax').parallax();
-  $(window).scroll(function() {
-        var distanceFromTop = $(document).scrollTop();
-        if (distanceFromTop >= $('.parallax-container').height())
-        {
-            $('.main__nav').addClass('fixed');
-        }
-        else
-        {
-            $('.main__nav').removeClass('fixed');
-        }
-    });
-});
+/*jshint esversion: 6 */
+
+// $(document).ready(function(){
+//   $('.parallax').parallax();
+//   $(window).scroll(function() {
+//         var distanceFromTop = $(document).scrollTop();
+//         if (distanceFromTop >= $('.parallax-container').height())
+//         {
+//             $('.main__nav').addClass('fixed');
+//         }
+//         else
+//         {
+//             $('.main__nav').removeClass('fixed');
+//         }
+//     });
+// });
+
+
+
+const current = document.querySelector('#current');
+const imgs = document.querySelectorAll('.imgs img');
+const opacity = 0.4;
+
+// e is the event paramter, changes as the target changes (each img clicked on)
+imgs.forEach(img => img.addEventListener('click', imgClick));
+
+function imgClick(e) {
+  // reset opacity
+  imgs.forEach(img => img.style.opacity = 1);
+
+  // change current img to the img most recently clicked
+  current.src = e.target.src;
+
+  // apply opacity to selected img
+  e.target.style.opacity = opacity;
+}
